f3fc75d3f528257a3e87562a745d6be52f8c4929
chouseknecht <chouseknecht@ansible.com>
2016-04-19 21:37:14 -0400

    Fixing doc strings. Removing state 'tagged'.

--- docker_images.py
+++ docker_images.py
@@ -41,6 +41,8 @@ options:
   archive_path:
     description:
       - Use with state 'present' to archive an image to a .tar file.
+    required: false
+    default: null
   config_path:
     description:
       - Path to the Docker CLI config file.
@@ -49,7 +51,6 @@ options:
     description:
       - Use with state 'present' to provide an alternate name for the Dockerfile to use when building an image.
     default: Dockerfile
-    version_added: "2.0"
   force:
     description:
       - Use with absent state to un-tag and remove all images matching the specified name. Use with states 'present'
@@ -60,6 +61,8 @@ options:
     description:
       - Timeout for HTTP requests during the image build operation. Provide a positive integer value for the number of
         seconds.
+    default: null
+    required: false
   name:
     description:
       - Image name. Name format will be one of: name, repository/name, registry_server:port/name.
@@ -71,6 +74,8 @@ options:
         Dockerfile for building an image.
     aliases:
       - build_path
+    default: null
+    required: false
   push:
     description:
       - Use with state present to always push an image to the registry. The image name must contain a repository
@@ -90,7 +95,9 @@ options:
     default: false
   repository:
     description:
-      - Use with state tagged to provide the full path to the repository.
+      - Full path to a repository. Use with state 'present' to tag the image into the repository.
+    required: false
+    default: null
   state:
     description:
       - Make assertions about the state of an image.
@@ -99,14 +106,12 @@ options:
       - When 'present' check if an image exists using the provided name and tag. If the image is not found or the
         force option is used, the image will either be pulled, built or loaded. By default the image will be pulled
         from Docker Hub. To build the image, provide a path value set to a directory containing a context and
-        Dockerfile. To load an image, specify load_path to provide a path to an archive file.
-      - Use 'tagged' to tag an image into a repository. Provide a repository value containing the repository path. Use
-        the force option to replace an existing image.
+        Dockerfile. To load an image, specify load_path to provide a path to an archive file. To tag an image to a
+        repository, provide a repository path.
     default: present
     choices:
       - absent
       - present
-      - tagged
   tag:
     description:
       - Used to select an image when pulling. Will be added to the image when pushing, tagging or building. Defaults to
@@ -118,7 +123,8 @@ requirements:
   - "docker-py"
 
 authors:
-  - Chris Houseknecht (house@redhat.com)
+  - Chris Houseknecht (@chouseknecht)
+  - James Tanner (@jtanner)
 
 '''
 
@@ -131,7 +137,6 @@ EXAMPLES = '''
 - name: Tag to repository in private registry
   docker_image:
     name: pacur/centos-7
-    state: tagged
     repository: registry.ansible.com/chouseknecht/centos_images
     tag: 7.0
     push: yes
@@ -159,6 +164,17 @@ EXAMPLES = '''
     name: registry.ansible.com/chouseknecht/sinatra
     tag: v1
     load_path: my_sinatra.tar
+'''
+
+RETURN = '''
+actions:
+    description: List of actions taken by the module.
+    returned: always
+    type: list
+    sample: [
+
+    ]
+
 '''
 
 
@@ -236,7 +252,7 @@ class ImageManager(DockerBaseClass):
                 self.results['changed'] = True
                 image = self.client.find_image(name=self.name, tag=self.tag)
                 if image:
-                    self.results['results'] = image
+                    self.results['image'] = image
 
             elif self.load_path:
                 # Load the image from an archive
@@ -266,7 +282,7 @@ class ImageManager(DockerBaseClass):
                 self.results['changed'] = True
                 image = self.client.find_image(self.name, self.tag)
                 if image:
-                    self.results['results'] = image
+                    self.results['image'] = image
 
             else:
                 # pull the image
@@ -277,7 +293,7 @@ class ImageManager(DockerBaseClass):
                 self.results['changed'] = True
                 image = self.client.find_image(name=self.name, tag=self.tag)
                 if image:
-                    self.results['results'] = image
+                    self.results['image'] = image
 
         if self.archive_path:
             self.archive_image(self.name, self.tag)
@@ -304,6 +320,7 @@ class ImageManager(DockerBaseClass):
 
             self.results['changed'] = True
             self.results['actions'].append("Removed image {0}".format(name))
+            self.results['image']['state'] = 'Deleted'
 
     def archive_image(self, name, tag):
         '''
@@ -338,7 +355,7 @@ class ImageManager(DockerBaseClass):
                 self.results['changed'] = True
                 image = self.client.find_image(name=name, tag=tag)
                 if image:
-                    self.results['results'] = image
+                    self.results['image'] = image
 
     def push_image(self, name, tag=None):
         '''
@@ -375,8 +392,8 @@ class ImageManager(DockerBaseClass):
             self.results['changed'] = True
             image = self.client.find_image(name=repository, tag=tag)
             if image:
-                self.results['results'] = image
-            self.results['results']['push_status'] = status
+                self.results['image'] = image
+            self.results['image']['push_status'] = status
         except Exception, exc:
             if re.search(r'unauthorized', str(exc)):
                 self.fail("Error pushing image {0}: {1}. Does the repository exist?".format(repository, str(exc)))
@@ -402,7 +419,7 @@ class ImageManager(DockerBaseClass):
                         raise Exception("Tag operation failed.")
                 image = self.client.find_image(name=self.repository, tag=self.tag)
                 if image:
-                    self.results['results'] = image
+                    self.results['image'] = image
             except Exception, exc:
                 self.fail("Error: failed to tag image {0} - {1}".format(self.name, str(exc)))
 
@@ -427,25 +444,19 @@ def main():
         push=dict(type='bool', default=False),
         repository=dict(type='str'),
         rm=dict(type='bool', default=True),
-        state=dict(type='str', choices=['absent', 'present', 'tagged'], default='present'),
+        state=dict(type='str', choices=['absent', 'present'], default='present'),
         tag=dict(type='str', default='latest'),
         )
-
-    required_if = [
-        ('state', 'tagged', ['repository', 'tag'])
-    ]
 
     client = AnsibleDockerClient(
         argument_spec=argument_spec,
         supports_check_mode=True,
-        required_if=required_if
     )
 
     results = dict(
         changed=False,
-        check_mode=client.check_mode,
         actions=[],
-        results={}
+        image={}
     )
 
     ImageManager(client, results)
